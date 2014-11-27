a608ea1113e2ede2be4da50dee9476522c323271
Eric Kort <eric.kort@gmail.com>
2014-11-27 16:58:47 -0500

    added R Pi IO functionality

--- routes/click.js
+++ routes/click.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var isPi = /^arm/.test(process.arch);
+var PIN = 23;
+var CLICK_LENGTH = 500; 
 
-/* GET users listing. */
+if(isPi)
+	var gpio = require("pi-gpio");
+
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+	if(isPi) {
+		gpio.open(PIN, "output", function(err) {
+		    gpio.write(PIN, 1, function() {  	  // click on     
+		    	setTimeout(function() {
+		    		gpio.write(PIN, 0, function() {  // click off
+				        gpio.close(PIN); 
+		    		});
+				}, CLICK_LENGTH);  // click is 500 ms long
+		    });
+		});
+	}
+	res.send(process.platform);
 });
 
 module.exports = router;
